341fdb5ea0ff741fd790cc34f2b67e68d547cd7c
Ricardo José Diaz Briceño <rjoseb11@gmail.com>
2019-04-02 17:09:33 -0500

    Exposed port in vggface-service

--- dockers/runmodels.sh
+++ dockers/runmodels.sh
@@ -12,14 +12,14 @@ if [[ "$(sudo docker images -q vggface:gpu 2> /dev/null)" != "" ]]
 then
 echo Image vggface already built
 else
-sudo docker build -t vggface:gpu vggface/ 
+sudo docker build -t vggface vggface/ 
 fi
 
 # Run the vggface container locally
-sudo docker run --runtime=nvidia --name=vggface-service --rm \
--dit -v /home/deepai/Documents/projects/facial-recognition-poc/dockers/vggface:/app vggface
+sudo docker run --name=vggface-service --runtime=nvidia --rm \
+-dit -v /home/deepai/Documents/projects/facial-recognition-poc/dockers/vggface:/app -p 7001:7000 vggface
 
 # Run the container in the port 7000
-sudo docker run --name=knn-service --rm --link=vggface-service \
+sudo docker run --name=knn-service --link=vggface-service --rm  \
 -dit -v /home/deepai/Documents/projects/facial-recognition-poc/dockers/knn:/app -p 7000:7000 knn
 
