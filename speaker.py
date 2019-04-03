8c68e579fd50541709a13c408c3177ecbef7738b
Ricardo José Diaz Briceño <rjoseb11@gmail.com>
2019-04-03 16:44:18 -0500

    Added json functionality

--- speaker.py
+++ speaker.py
@@ -1,45 +1,49 @@
 from datetime import datetime
 
 import win32com.client as wincl
+from datetime import datetime
+
 import pandas as pd
 import numpy as np
 
 import mysql.connector
-import time
+import json
 
 def speak(msg):
+    print(msg)
     speak = wincl.Dispatch("SAPI.SpVoice")
     speak.Speak(msg)
 
-messages = {
-    "Ricardo":"Hola Ricard, bienvenido al Show Room", 
-    }
+responses_file = "utils/responses.json"
 
-t = time.time()
+print("Listening..")
 while True:
     conn =  mysql.connector.connect(
             host="localhost",
             user="admin",
             passwd="admin",
-            database="facedb",
-    )
+            database="facedb")
+
     df = pd.read_sql("SELECT Name, FirstSeen, max(LastSeen) as LastSeen FROM recognition GROUP BY Name ORDER BY LastSeen desc", conn)
 
+    messages = json.load(open(responses_file, "rb"))
+
     for i, row in df.iterrows():
-        first_seen = datetime.strptime(row["FirstSeen"], ('%d/%m/%y %H:%M:%S'))
-        last_seen = datetime.strptime(row["LastSeen"], ('%d/%m/%y %H:%M:%S'))
+        first_seen = datetime.strptime(row["FirstSeen"], '%d/%m/%y %H:%M:%S')
+        last_seen = datetime.strptime(row["LastSeen"], '%d/%m/%y %H:%M:%S')
 
         difference = (last_seen-first_seen).seconds
 
-        if difference < 10:
-            if messages.get(row["Name"], 0):
-                speak(messages[row["Name"]])
-                messages.pop(row["Name"])
+        if difference < 20 and messages["new_messages"].get(row["Name"], 0):
+            name = row["Name"]
+            message = messages["new_messages"][name]
+            time_now = datetime.now().strftime('%d/%m/%y %H:%M:%S')
 
-    et = int(time.time()-time)
+            speak(message)
+            messages["log"] = {name: {"message":message, 
+                                      "time":time_now}}
+            messages["new_messages"].pop(row["Name"])
+            
+            json.dump(messages, open(responses_file, "w"))
 
     conn.close()
-
-    print("{} seconds".format(et), end="\r")
-    if et > 120:
-        break
