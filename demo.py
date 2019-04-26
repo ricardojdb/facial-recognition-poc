303fbf35f1f727e3254cd1593a298ef11268722b
Ricardo José Diaz Briceño <rjoseb11@gmail.com>
2019-04-26 12:00:26 -0500

    Improved face recognition
    Widen the detection box to capture the whole face,
    this improves the recognition significantly.

--- demo.py
+++ demo.py
@@ -15,7 +15,7 @@ net = cv2.dnn.readNetFromCaffe("models/prototxt.txt", "models/res10_300x300_ssd_
 sql_utils.create_mysql_table()
 recog_dict = {}
 # Start thread to capture and show the stream.
-video_path = 0 
+video_path = 0
 video_capture = utils.WebcamVideoStream(video_path).start()
 
 host = "localhost"
@@ -51,10 +51,13 @@ while True:
         if confidence > 0.2:
             # compute the (x, y)-coordinates of the bounding box 
             box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
-            (startX, startY, endX, endY) = box.astype("int")
+            (xmin, ymin, xmax, ymax) = box.astype("int")
+            # Widen the box so we can capture the whole face
+            xmin_wide, ymin_wide, xmax_wide, ymax_wide = utils.get_wide_box(
+                w, h, xmin, ymin, xmax, ymax)
 
             # cut face from the frame
-            roi_color = frame[startY:endY, startX:endX]
+            roi_color = frame[ymin_wide:ymax_wide, xmin_wide:xmax_wide]
 
             # if no face is detected conitnue to the next one
             if not roi_color.size: continue
@@ -70,7 +73,7 @@ while True:
 
             # gather the data that's going to be passed to
             # the WebcamVideoStream to plot the bounding box
-            data.append([outputs['label'], outputs['dist'], (startX,startY,endX-startX,endY-startY)])
+            data.append([outputs['label'], outputs['dist'], (xmin,ymin,xmax-xmin,ymax-ymin)])
             
             # if label is Unknown don't add the data to the MySQL database
             if outputs['label'] == "Unknown": continue
